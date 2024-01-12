165e2f9248c423696812544c4790446e7bc1348d
Chandan <hb7102849@gmail.com>
2024-01-12 15:53:51 +0530

    add post

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,7 @@ const PORT=process.env.PORT || 5000;
 app.use(
   cors({
       origin:["https://localhost:5000","https://siya-ram-store.vercel.app"],
+      methods: ["post","get"],
       credentials:true,
   })
 )
